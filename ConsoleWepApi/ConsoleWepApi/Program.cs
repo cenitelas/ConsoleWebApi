e76fb0955215be2d8f815fba359a3655330b5489
АтчановА <@DESKTOP-GG3G2B2>
2019-09-19 19:54:30 +0600

    create project

---
+++ ConsoleWepApi/ConsoleWepApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWepApi
+{
+    public class AuthorModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:60719/api/AuthorApi");
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    string result = rd.ReadToEnd();
+                    List<AuthorModel> authors = JsonConvert.DeserializeObject<List<AuthorModel>>(result);
+                    foreach (var item in authors)
+                    {
+                        Console.WriteLine(item.Id + "\t" + item.FirstName + "\t" + item.LastName);
+                    }
+                }
+            }
+        }
+    }
+}
