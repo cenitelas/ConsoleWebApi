cdc5eccd5e97ad6eba8e5aba10c80cd1a37de497
Александр <cenitelas@mail.ru>
2019-09-25 23:07:10 +0600

    release

---
+++ ConsoleWepApi/ConsoleWepApi/BookModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWepApi
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string Title { get; set; }
+
+        public int? Pages { get; set; }
+
+        public int? Price { get; set; }
+    }
+}
--- ConsoleWepApi/ConsoleWepApi/Program.cs
+++ ConsoleWepApi/ConsoleWepApi/Program.cs
@@ -11,52 +11,87 @@ using System.Threading.Tasks;
 
 namespace ConsoleWepApi
 {
-    public class AuthorModel
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
+        private static List<UserModel> users;
+        private static List<BookModel> books;
+        private static List<AuthorBook> userBooks;
         static void Main(string[] args)
         {
+           
             var menu = "0";
-            do { 
-                Console.WriteLine("1> Добавить автора.");
-                Console.WriteLine("2> Список авторов.");
-                Console.WriteLine("3> Удалить автора");
+            do
+            {
+                Task load = LoadProgram();
+                load.Wait();
+                Console.WriteLine("1> Выдать книгу.");
+                Console.WriteLine("2> Вернуть книгу.");
+                Console.WriteLine("3> Список должников");
                 Console.WriteLine("0> Выход.");
                 Console.Write(">");
                 menu = Console.ReadLine();
 
-                switch (menu) {
+                switch (menu)
+                {
                     case "1":
                         Console.Clear();
-                        AuthorModel newAuthor = new AuthorModel();
-                        Console.Write("Имя: ");
-                        newAuthor.FirstName = Console.ReadLine();
-                        Console.Write("Фамилия: ");
-                        newAuthor.LastName = Console.ReadLine();
-                        CreateAuthor(newAuthor);
+                        AuthorBook authorBook = new AuthorBook();
+                        PrintBooks();
+                        Console.WriteLine();
+                        Console.Write("Введите номер книги (0 - выход): ");
+                        int answer = 0;
+                        int.TryParse(Console.ReadLine(), out answer);
+                        if (answer == 0)
+                            break;
+                        authorBook.BooksId = answer;
+                        Console.Clear();
+                        PrintUsers();
+                        Console.WriteLine();
+                        Console.Write("Введите номер пользователя (0 - выход): ");
+                        int answer2 = 0;
+                        int.TryParse(Console.ReadLine(), out answer2);
+                        if (answer2 == 0)
+                            break;
+                        authorBook.UserId = answer2;
+                        authorBook.AuthorId = books.Find(i => i.Id == authorBook.BooksId).AuthorId;
+                        authorBook.AuthorName = books.Find(i => i.Id == authorBook.BooksId).AuthorName;
+                        authorBook.UserName = users.Find(i => i.Id == authorBook.UserId).Name;
+                        authorBook.BooksName = books.Find(i => i.Id == authorBook.BooksId).Title;
+                        Console.Clear();
+                        Console.Write("Введите количество дней аренды (0 - выход): ");
+                        int answer3 = 0;
+                        int.TryParse(Console.ReadLine(), out answer3);
+                        if (answer3 == 0)
+                            break;
+                        authorBook.DateOrder = DateTime.Now.AddDays(answer3);
+                        CreateUserBook(authorBook);
+                        Console.Clear();
+                        Console.WriteLine("Заказ успешно оформлен!");
+                        Console.ReadKey();
                         break;
                     case "2":
                         Console.Clear();
-                        LoadContent();
+                        PrintUsersBooks();
+                        Console.WriteLine();
+                        Console.Write("Введите номер заказа (0 - выход): ");
+                        int answer4 = 0;
+                        int.TryParse(Console.ReadLine(), out answer4);
+                        if (answer4 == 0)
+                            break;
+                        DeleteUserBook(answer4);
+                        Console.Clear();
+                        Console.WriteLine("Заказ успешно удален!");
                         Console.ReadKey();
                         break;
                     case "3":
                         Console.Clear();
-                        LoadContent();
-                        Console.Write("\n\nnВведите Id Автора из списка:");
-                        var id = 0;
-                        int.TryParse(Console.ReadLine(),out id);
-                        DeleteAuthor(id);
+                        PrintUsersBooksDebtor();
+                        Console.ReadKey();
                         break;
                     default:
                         break;
-                        
+
                 }
                 Console.Clear();
             } while (!menu.Equals("0"));
@@ -64,28 +99,108 @@ namespace ConsoleWepApi
 
         }
 
-
-        static async void LoadContent()
+        static async Task<bool> LoadProgram()
         {
-            var response = await client.GetAsync("http://localhost:60719/api/AuthorApi");
-
-            List<AuthorModel> authors = JsonConvert.DeserializeObject<List<AuthorModel>>(response.Content.ReadAsStringAsync().Result);
-            foreach (var item in authors)
-            {
-                Console.WriteLine(item.Id + "\t" + item.FirstName + "\t" + item.LastName);
-            }
-          
+            var response = await client.GetAsync("http://localhost:60719/api/UserApi");
+            users = JsonConvert.DeserializeObject<List<UserModel>>(response.Content.ReadAsStringAsync().Result);
+            response = await client.GetAsync("http://localhost:60719/api/BookApi");
+            books = JsonConvert.DeserializeObject<List<BookModel>>(response.Content.ReadAsStringAsync().Result);
+            response = await client.GetAsync("http://localhost:60719/api/UserBooksApi");
+            userBooks = JsonConvert.DeserializeObject<List<AuthorBook>>(response.Content.ReadAsStringAsync().Result);
+            return true;
         }
 
-        static async void CreateAuthor(AuthorModel obj)
+        static void PrintBooks()
+        {
+            var formatString = string.Format("{{0, -{0}}}", 10);
+            Console.Write(formatString, "№");
+            Console.Write(formatString, "Title");
+            Console.Write(formatString, "Author");
+            Console.Write(formatString, "Genre");
+            Console.Write(formatString, "Pages");
+            Console.Write(formatString, "Price");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in books)
+            {
+                Console.Write(formatString, s.Id);
+                Console.Write(formatString, s.Title);
+                Console.Write(formatString, s.AuthorName);
+                Console.Write(formatString, s.GenreName);
+                Console.Write(formatString, s.Pages);
+                Console.Write(formatString, s.Price);
+                Console.WriteLine();
+            }         
+        }
+
+        static void PrintUsers()
+        {
+            var formatString = string.Format("{{0, -{0}}}", 10);
+            Console.Write(formatString, "№");
+            Console.Write(formatString, "Name");
+            Console.Write(formatString, "Email");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in users)
+            {
+                Console.Write(formatString, s.Id);
+                Console.Write(formatString, s.Name);
+                Console.Write(formatString, s.Email);
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintUsersBooks()
+        {
+            var formatString = string.Format("{{0, -{0}}}", 10);
+            Console.Write(formatString, "№");
+            Console.Write(formatString, "Title");
+            Console.Write(formatString, "Author");
+            Console.Write(formatString, "User");
+            Console.Write(formatString, "Date return");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in userBooks)
+            {
+                Console.Write(formatString, s.Id);
+                Console.Write(formatString, s.BooksName);
+                Console.Write(formatString, s.AuthorName);
+                Console.Write(formatString, s.UserName);
+                Console.Write(formatString, s.DateOrder.ToShortDateString());
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintUsersBooksDebtor()
+        {
+            var formatString = string.Format("{{0, -{0}}}", 10);
+            Console.Write(formatString, "№");
+            Console.Write(formatString, "Title");
+            Console.Write(formatString, "Author");
+            Console.Write(formatString, "User");
+            Console.Write(formatString, "Date return");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var s in userBooks.Where(i=>i.DateOrder<DateTime.Now))
+            {
+                Console.Write(formatString, s.Id);
+                Console.Write(formatString, s.BooksName);
+                Console.Write(formatString, s.AuthorName);
+                Console.Write(formatString, s.UserName);
+                Console.Write(formatString, s.DateOrder.ToShortDateString());
+                Console.WriteLine();
+            }
+        }
+
+        static async void CreateUserBook(AuthorBook obj)
         {
             HttpContent content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
-            await client.PostAsync("http://localhost:60719/api/AuthorApi", content);
+            await client.PostAsync("http://localhost:60719/api/UserBooksApi", content);
         }
 
-        static async void DeleteAuthor(int id)
+        static async void DeleteUserBook(int id)
         {
-          await client.DeleteAsync("http://localhost:60719/api/AuthorApi/"+id);
+          await client.DeleteAsync("http://localhost:60719/api/UserBooksApi/" + id);
         }
     }
 }
